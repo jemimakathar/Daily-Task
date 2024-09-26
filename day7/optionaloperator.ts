55f1c73beb2695c45b273e033e9d62c6e496405d
jemimakathar <katharolijemima@gmail.com>
2024-09-26 14:11:02 +0530

    uploaded file

---
+++ day7/optionaloperator.ts
@@ -0,0 +1,48 @@
+interface User {
+    id: number;
+    name: string;
+    address?: {
+        city?: string;
+        country: string;
+    };
+}
+const users: User = { id: 1, name: "Alice", address: { country: "USA" } };
+const city = users.address?.city; 
+console.log(city); 
+const country = users.address?.country; 
+console.log(country); 
+console.log("---------------------");
+
+
+
+
+
+
+interface Review {
+    reviewer: string;
+    rating: number;
+    comment?: string;  
+    date?: Date;       
+}
+
+interface Product {
+    id: number;
+    name: string;
+    price: number;
+    reviews?: Review[]; 
+}
+const product: Product = {
+    id: 1,
+    name: "Smartphone",
+    price: 699,
+    reviews: [
+        { reviewer: "Alice", rating: 5, comment: "Great phone!" },
+        { reviewer: "Bob", rating: 4 } // Comment is optional
+    ]
+};
+const firstReviewComment = product.reviews?.[0]?.comment;
+console.log(firstReviewComment); // Output: "Great phone!"
+
+const secondReviewComment = product.reviews?.[1]?.comment; 
+console.log(secondReviewComment); 
+
