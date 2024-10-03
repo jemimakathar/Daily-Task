30465f7203bc8ae44fa9c76847e1c98da7d0a342
jemimakathar <katharolijemima@gmail.com>
2024-10-03 14:19:28 +0530

    uploadede file

--- day8/getter_setter.ts
+++ day8/getter_setter.ts
@@ -1,16 +1,19 @@
 class student{
-    name:string="diya"
+    name:string="jemima"
     id:number=1234
     age:number=18
-
+   
 get get_name(){
     return this.name
 }
-set set_name(last_name:string)
-{
-    
+set set_name(final_name:string){
+    this.name=final_name
 }
 }
+
+const Student=new student
+Student.set_name="jemi"
+console.log(Student.get_name);
 
 
 // Create a book class with a private property_title.Implememt a getter and setter for the title.
