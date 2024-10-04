94559c1262432db784b35a6d0f4a0a1a3fa85c78
jemimakathar <katharolijemima@gmail.com>
2024-10-04 15:15:06 +0530

    uploded file

---
+++ day6/import.ts
@@ -0,0 +1,12 @@
+import { Parent } from "./module";
+class Child_class extends Parent{
+   Name="jemi";
+   Address="abc nagar";
+   phone="243546"
+
+} 
+const c = new Child_class;
+console.log(c.Address);
+console.log(c.Name);
+console.log(c.phone);
+
---
+++ day6/module.ts
@@ -0,0 +1,5 @@
+export class Parent{
+ public Name:string;
+ private Age:number;
+  protected Address:string;
+}
